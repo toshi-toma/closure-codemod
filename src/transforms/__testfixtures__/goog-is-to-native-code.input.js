9dce0df04522c25872d239e1e3d937f81c12b2b3
toshi-toma <toshitoma1217@gmail.com>
2020-07-27 10:47:53 +0900

    feat: define base file goog-is-to-native-code

---
+++ src/transforms/__testfixtures__/goog-is-to-native-code.input.js
@@ -0,0 +1,3 @@
+const foo = 0;
+
+goog.isDef(foo);
--- src/transforms/__tests__/index.spec.ts
+++ src/transforms/__tests__/index.spec.ts
@@ -1,3 +1,4 @@
 const { defineTest } = require("jscodeshift/dist/testUtils");
 
 defineTest(__dirname, "goog-provide-to-goog-module");
+defineTest(__dirname, "goog-is-to-native-code");
