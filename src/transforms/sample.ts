cbddb3421f21e79c84e32e6dcdbbcedcf9d75603
toshi-toma <toshitoma1217@gmail.com>
2020-07-27 15:26:21 +0900

    chore: format

--- src/transforms/sample.ts
+++ src/transforms/sample.ts
@@ -2,6 +2,6 @@ import { FileInfo, Transform, API, Options } from "jscodeshift";
 
 const transform: Transform = (file: FileInfo, api: API, options: Options) => {
   return file.source;
-}
+};
 
 export default transform;
