2ab134bfd8ce1a6b396a1bdc8dfdc5da62dfa423
toshi-toma <toshitoma1217@gmail.com>
2020-08-31 14:59:34 +0900

    test: add tests

--- transforms/__testfixtures__/goog-json-to-native-code/goog-json-parse.output.js
+++ transforms/__testfixtures__/goog-json-to-native-code/goog-json-parse.output.js
@@ -1,1 +1,29 @@
-const obj = JSON.parse("{}");
+/**
+ * goog.json.parse -> `JSON.parse`
+ */
+
+// test
+const isDef = JSON.parse("{}");
+
+if (JSON.parse("{}")) {
+  console.log("foo");
+} else if (JSON.parse("{\"bar\": 0}").bar) {
+  console.log("bar");
+} else {
+  console.log("bar");
+}
+
+const def = JSON.parse("{\"bar\": 0}").bar ? "foo" : "bar";
+
+goog.asserts.assert(JSON.parse("{\"bar\": 0}").bar);
+goog.asserts.assert(!JSON.parse("{\"bar\": 0}").bar || foo !== "bar");
+
+const f = () => {
+  return JSON.parse("{\"bar\": 0}").bar;
+};
+
+const o = {
+  s: JSON.parse("{\"bar\": 0}").bar ? "foo" : "",
+};
+
+m("method", JSON.parse("{\"bar\": 0}").bar ? "foo" : "");
